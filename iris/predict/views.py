86fc67d0eb0f5ad89076108c8091039bd3fb7980
RobinsonLuzo <bendanielfagan@gmail.com>
2021-06-30 11:24:01 +0100

    predict_chances view implemented - without results being added to database. Returned as a JSON response.

--- iris/predict/views.py
+++ iris/predict/views.py
@@ -1,5 +1,29 @@
+import joblib
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Prediction view
 def predict(request):
     return render(request, 'predict.html')
+
+# prediction handling - on submission of data by user:
+def predict_chances(request):
+
+    # if POST, assign data recieved from client:
+    if request.POST.get('action') == 'post':
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width  = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width  = float(request.POST.get('petal_width'))
+
+        model = joblib.load(r"C:\Users\BenDa\Desktop\Projects\ML_Deployment_with_Django\saved_model\iris_prediction.pkl")
+
+        # Make prediction
+        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+        classification = result[0]
+
+        return JsonResponse({'result': classification, 'sepal_length': sepal_length,
+                             'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
+                            safe=False)
+
